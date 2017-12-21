522a3121b982c0264a28969a5e9a4e0ba4cf627e
edmundex <8471411+edmundex@users.noreply.github.com>
2017-12-21 06:50:32 -0800

    Update build_D3partitionR.R

--- R/build_D3partitionR.R
+++ R/build_D3partitionR.R
@@ -311,6 +311,8 @@ compile_D3_partitionR<-function(D3partitionR_object)
   ###Color scale####
 
   ##If no color variable have been provided, default to name
+  
+  print(d3$color)
 
   ##if no color scale have been supplied
   if (is.null(d3$color))
